0d06990c43d8bba3afe494fd9e9bbb76e8ad6977
davidgreen29 <63319616+davidgreen29@users.noreply.github.com>
2022-08-26 09:43:35 -0400

    main page content paragraph text edit

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,11 +6,11 @@ export default function MainContent() {
             
                 <h1 className="page-headline">Let's Build Together!</h1>
             
-            <p className="home-page-para-content">Hello, I‘m David <span className="green">Green</span> a developer.
-             Are you looking for someone to help build a new application with React or 
-             maintain an existing one? I'm your guy. Do you need help with maintaining
-             your NoSQL database or realational database? I'm your guy for that too.
-             I'll also add if you need help with responsive CSS styles look no farther. 
+            <p className="home-page-para-content">Hello, I‘m David <span className="green">Green</span>,
+            looking for someone to help build a new application with React or maintain an existing one? 
+            Maybe you need help with responsive CSS styles, well look no farther I'm your guy. Do you 
+            have a NoSQL or relational database? I'm your guy for that too. Have any other questions 
+            about how I could help improve an application you're working on, feel free to contact me.
              </p> 
 
            
