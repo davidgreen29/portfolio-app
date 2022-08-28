ee525c586489b6ea8c21ef90fdce91a92e707c81
davidgreen29 <63319616+davidgreen29@users.noreply.github.com>
2022-08-28 13:13:21 -0400

    about and main content info text edit

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -7,9 +7,9 @@ export default function MainContent() {
                 <h1 className="page-headline">Let's Build Together!</h1>
             
             <p className="home-page-para-content">Hello, I‘m David <span className="green">Green</span>,
-            looking for someone to help build a new application with React or maintain an existing one? 
-            Maybe you need help with responsive CSS styles, well look no farther I'm your guy. Do you 
-            have a NoSQL or relational database? I'm your guy for that too. Have any other questions 
+            are you looking for someone to help build a new application with React or maintain an existing code base, 
+            maybe you need help with responsive CSS styles, well look no further. Do you 
+            have a NoSQL or relational database, I can help with that too. Have any other questions 
             about how I could help improve an application you're working on, feel free to contact me.
              </p> 
 
--- src/components/Navbar/About.js
+++ src/components/Navbar/About.js
@@ -18,12 +18,12 @@ export default function About() {
     <h1 className="about-title">About</h1>
     <p className="about-info">
         My developer journey started quite a few years ago when I had to take engineering
-        fundamentals class were we had to learn Matlab. I instantly new that programming 
+        fundamentals class where we had to learn Matlab. I instantly new that programming 
         was something I would never be bored doing. I love that there is always 
         a new problem to solve and something to learn. Software development is 
         one of the few fields that exist were a person can be as good as they 
         desire. All the info to learn any new language, framework, library, or fundamental
-        concept is freely available online. All one needs is a passion for Software
+        concept is freely available online. All one needs is a passion for software
         development and lifelong learning, which perfectly describes me.  
          
     </p>
