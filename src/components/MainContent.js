ab66eab2bc92cb4775c40e4341952e74c48c86c1
davidgreen29 <63319616+davidgreen29@users.noreply.github.com>
2022-10-25 11:42:33 -0400

    "main content description change"

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -7,11 +7,10 @@ export default function MainContent() {
                 <h1 className="page-headline">Let's Build Together!</h1>
             
             <p className="home-page-para-content">Hello, I'm David <span className="green">Green</span>.
-            Are you looking for someone to help build a new application with React or maintain an existing 
-            code base? Maybe you need help with responsive CSS styles? Do you have a NoSQL or relational
-             database? Well, search no further; I can help with all these things. If you'd like to know how
-
-            I could help improve an application you're working on, feel free to contact me or view my work 
+            Are you looking for someone to help build a new application or maintain an existing 
+            code base? Does your app use javascript, react.js, node.js, html, ccs, sql, or C#? 
+            Well, search no further; I can help with all these things. If you'd like to know how
+            I could help improve an application you're working on, feel free to contact me or view my personal projects
             on <a className="para-link-git" href="https://github.com/davidgreen29" target="_blank" rel="noopener noreferrer">Github</a>.
              </p> 
 
