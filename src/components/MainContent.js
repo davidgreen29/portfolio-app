9c1e2eb5858a143b244058ae2935fc879995a385
davidgreen29 <63319616+davidgreen29@users.noreply.github.com>
2022-11-30 19:40:33 -0500

    "main content text edit"

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,7 +8,7 @@ export default function MainContent() {
             
             <p className="home-page-para-content">Hello, I'm David <span className="green">Green</span>.
             Are you looking for someone to help build a new application or maintain an existing 
-            code base? Does your app use javascript, react.js, node.js, html, ccs, sql, or C#? 
+            code base? Does your app use JavaScript, React.js, Node.js, HTML, and CSS, or C# and .NET?
             Well, search no further; I can help with all these things. If you'd like to know how
             I could help improve an application you're working on, feel free to contact me or view my personal projects
             on <a className="para-link-git" href="https://github.com/davidgreen29" target="_blank" rel="noopener noreferrer">Github</a>.
