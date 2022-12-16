451b4ed2f79d459ed6226e58971bfd445b52b403
davidgreen29 <63319616+davidgreen29@users.noreply.github.com>
2022-12-16 11:15:34 -0500

    "main content text edit"

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,8 +6,8 @@ export default function MainContent() {
             
                 <h1 className="page-headline">Let's Build Together!</h1>
             
-            <p className="home-page-para-content">
-            Software development is about more than just writing code. It's about solving problems and using your tools to 
+            <p className="home-page-para-content">Hello, I'm David <span className="green">Green</span>.
+             Software development is about more than just writing code. It's about solving problems and using your tools to 
             do that. As John Johnson said, "First solve the problem, then write the code." Solving problems is one of my 
             favorite things to do and where I get my motivation. I'm always looking for the next challenge. If you've got a problem to solve,
              feel free to contact me or view my personal projects
